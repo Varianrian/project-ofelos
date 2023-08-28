5be6f72d4bc5d96aaecbb13ad15bffa0963cb5ec
Varian Avila Faldi <varian236@gmail.com>
2023-08-28 23:27:21 +0700

    fix toaster bug

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,10 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
+import toast, { Toaster } from "react-hot-toast";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>{children}</body>
+      <Toaster />
     </html>
   );
 }
