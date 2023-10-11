b1fdc61eea23a5823470f1690d07ef233ea793a5
Varian Avila Faldi <varian236@gmail.com>
2023-10-11 21:23:19 +0700

    test iframe with youtube

--- src/components/MainPage/CameraContainer.jsx
+++ src/components/MainPage/CameraContainer.jsx
@@ -5,7 +5,13 @@ function CameraContainer() {
     <>
       <div className='flex justify-center'>
         <div className='w-[586px] h-[400px] bg-slate-800 rounded-3xl m-16'>
-          {/* Content */}
+        {/* <iframe src='http://172.20.10.2'></iframe> */}
+        <iframe width="560" height="315" 
+        src="https://www.youtube.com/embed/TePhNEJr9mg?si=OCgtQCxz6ZcXXHrk" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        allowfullscreen></iframe>
         </div>
       </div>
     </>
